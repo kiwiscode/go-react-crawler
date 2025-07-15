80a2076a2750383ce0b819adc1f05f20d21b5acf
kiwiscode <ayktkav@gmail.com>
2025-07-15 19:16:56 +0200

    Adjust Vite config server settings for Docker usage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
